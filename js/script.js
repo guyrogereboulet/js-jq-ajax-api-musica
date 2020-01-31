41fb12b70a9d85c1a3f03cf8a8dcc7602a1aca58
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-31 15:29:36 +0100

    Struttura base OK

--- js/script.js
+++
@@ -1,5 +0,0 @@
-// Attraverso una chiamata ajax all’Api di Boolean avremo a disposizione una decina di dischi musicali.
-// Servendoci di handlebars stampiamo tutto a schermo.
-// In questo momento non è importante la parte graﬁca.
-// Creare una select con i seguenti generi: pop, rock, metal e jazz.
-// In base a cosa scegliamo nella select vedremo i corrispondenti cd.
