1d5bc05dbeb67d4fe07d5d4805ea3ce80bfc26f3
Guy Roger Eboulet <eboulet87@gmail.com>
2020-02-02 23:47:33 +0100

    Select non funzionante

--- js/main.js
+++ js/main.js
@@ -8,24 +8,21 @@ $(document).ready(function() {
     "url" : url,
     "method" : "GET",
     "success" : function(data) {
-      console.log(data);
-      console.log(data.response);
       processData(data.response);
     },
     "error" : function(request, state, errors) {
       alert("errore" + errors);
     },
-  }
- );
+  });
+
 });
 
 function processData(cds) {
  for (var i = 0; i < cds.length; i++) {
    var cd = cds[i];
-   console.log(cd);
    var source = $("#entry-template").html();
    var template = Handlebars.compile(source);
    var html = template(cd);
-   $(".cds-container.container").append(html)
+   $(".cds-container").append(html)
  }
 }
