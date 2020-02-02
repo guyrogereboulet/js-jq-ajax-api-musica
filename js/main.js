6a700426018a0ef400235315f152d10a400ac4d1
Guy Roger Eboulet <eboulet87@gmail.com>
2020-02-02 16:19:26 +0100

    Inserimento select

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
 	//Code
+
+  var url = "https://flynn.boolean.careers/exercises/api/array/music";
+
+  $.ajax (
+  {
+    "url" : url,
+    "method" : "GET",
+    "success" : function(data) {
+      console.log(data);
+      console.log(data.response);
+      processData(data.response);
+    },
+    "error" : function(request, state, errors) {
+      alert("errore" + errors);
+    },
+  }
+ );
 });
+
+function processData(cds) {
+ for (var i = 0; i < cds.length; i++) {
+   var cd = cds[i];
+   console.log(cd);
+   var source = $("#entry-template").html();
+   var template = Handlebars.compile(source);
+   var html = template(cd);
+   $(".cds-container.container").append(html)
+ }
+}
